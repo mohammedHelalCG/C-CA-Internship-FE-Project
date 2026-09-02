c298ef35e785b1019242a6ad7df33c68796be232
najwa-hasan <najwa.hassan.ext@capgemini.com>
2026-09-02 15:57:03 +0300

    meal card and meal-list design

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,8 +12,11 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Aura
-      }
-    })
-  ]
+        preset: Aura,
+        options: {
+          darkModeSelector: 'none',
+        },
+      },
+    }),
+  ],
 };
