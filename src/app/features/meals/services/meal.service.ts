4fd57959f5b6982a1b9674624498d194cb467cef
najwa-hasan <najwa.hassan.ext@capgemini.com>
2026-09-03 13:37:35 +0300

    fix-pr-comments

--- src/app/features/meals/services/meal.service.ts
+++ src/app/features/meals/services/meal.service.ts
@@ -7,47 +7,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class MealService {
-  private apiUrl = 'http://localhost:8080/meals';
+  private apiUrl = 'https://railway.app';
   constructor(private http: HttpClient) {}
-
-  private meals: Meal[] = [
-    {
-      id: 1,
-      name: 'Chicken Burger',
-      category: 'Main Course',
-      price: 350,
-      description: 'Chicken burger with cheese',
-      image: '',
-      dietaryClassification: 1,
-    },
-    {
-      id: 2,
-      name: 'Pasta Alfredo',
-      category: 'Main Course',
-      price: 500,
-      description: 'Pasta white sauce',
-      image: '',
-      dietaryClassification: 2,
-    },
-    {
-      id: 3,
-      name: 'Salad',
-      category: 'Appetizer',
-      price: 190,
-      description: 'caesar salad',
-      image: '',
-      dietaryClassification: 3,
-    },
-    {
-      id: 4,
-      name: 'Fries',
-      category: 'Appetizer',
-      price: 120,
-      description: 'French fries with a side of a dip',
-      image: '',
-      dietaryClassification: 4,
-    },
-  ];
 
   getAllMeals(): Observable<Meal[]> {
     return this.http.get<Meal[]>(this.apiUrl);
