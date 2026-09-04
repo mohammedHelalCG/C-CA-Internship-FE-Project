274219935e9d2a9ae12c66a261117517fff60f04
Wello200 <aliw7112@gmail.com>
2026-09-04 20:36:50 +0300

    Fix commit

--- src/app/features/login/services/login.service.ts
+++ src/app/features/login/services/login.service.ts
@@ -14,18 +14,16 @@ export class LoginService {
 
 
   private users = [
-    {
-      email: 'Welo@capgemini.com',
-      password: 'password',
-    },
+    { email: 'Welo@capgemini.com', password: 'password' },
+    { email: 'Helal@capgemini.com', password: 'password' },
+    { email: 'Najwa@capgemini.com', password: 'password' },
+
   ];
   // authenticate(user: User): Observable<any> {
   //   return this.http.post(this.baseUrl + this.apiUrl, user);
   // }
-
-  authenticate(user: User): boolean {
-    return this.users.some(
-      u => u.email === user.email && u.password === user.password
-    );
+  authenticate(email: string, password: string): boolean {
+    const user = this.users.find(u => u.email === email && u.password === password);
+    return !!user;
   }
 }
