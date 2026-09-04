8907457e6f68718ca2f066c8111c6c2219a0206a
Wello200 <aliw7112@gmail.com>
2026-09-04 20:10:51 +0300

    Interceptor and guards

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,8 @@ import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from '@core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -18,5 +20,6 @@ export const appConfig: ApplicationConfig = {
         },
       },
     }),
+    provideHttpClient(withInterceptors([authInterceptor]))
   ],
 };
--- src/app/features/login/services/login.service.ts
+++ src/app/features/login/services/login.service.ts
@@ -1,19 +1,31 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '@shared/interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
+  private baseUrl = 'https://c-ca-internship-backend-project-production.up.railway.app/';
+  private apiUrl = 'login';
+
+  private http = inject(HttpClient)
+
 
   private users = [
-    { email: 'Welo@capgemini.com', password: 'password' },
-    { email: 'Helal@capgemini.com', password: 'password' },
-    { email: 'Najwa@capgemini.com', password: 'password' },
-
+    {
+      email: 'Welo@capgemini.com',
+      password: 'password',
+    },
   ];
+  // authenticate(user: User): Observable<any> {
+  //   return this.http.post(this.baseUrl + this.apiUrl, user);
+  // }
 
-  authenticate(email: string, password: string): boolean {
-    const user = this.users.find(u => u.email === email && u.password === password);
-    return !!user;
+  authenticate(user: User): boolean {
+    return this.users.some(
+      u => u.email === user.email && u.password === user.password
+    );
   }
 }
